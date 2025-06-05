993791267d9289fb740c923657e4a62dd27f6ac7
João Nhavene <jmnhavene2014@gmail.com>
2025-06-05 10:32:44 +0200

    first commit

---
+++ mgca/src/main/java/bau/dev/mgca/MgcaApplication.java
@@ -0,0 +1,13 @@
+package bau.dev.mgca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcaApplication.class, args);
+	}
+
+}
---
+++ mgca/src/test/java/bau/dev/mgca/MgcaApplicationTests.java
@@ -0,0 +1,13 @@
+package bau.dev.mgca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
